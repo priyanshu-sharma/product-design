1a708bf7595c45d233554a738f05f01e8818acc5
SuperThinking <superdhawan@gmail.com>
2022-01-31 17:06:36 +0530

    Add alembic - Minor refactors

--- src/product_design_model/server_config/settings.py
+++ src/product_design_model/server_config/settings.py
@@ -1,15 +1,29 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DEFAULT_TIMEOUT_SECONDS = 10
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # Database
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "product_design",
         "HOST": "localhost",
         "PORT": 5432,
+        "USER": "postgres",
+        "PASSWORD": "postgres"
     }
 }
+
+DATABASE = DATABASES["default"]
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE['USER']}:{DATABASE['PASSWORD']}@{DATABASE['HOST']}:{DATABASE['PORT']}/{DATABASE['NAME']}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEFAULT_TIMEOUT_SECONDS = 10
