9a059c72dba264d98da515fff1319188e9961a67
Priyanshu <priyanshu.sharma@grofers.com>
2022-01-31 10:33:10 +0530

    added sqlalchmey configuration and alembic

--- src/product_design_model/server_config/__init__.py
+++ src/product_design_model/server_config/__init__.py
@@ -1,1 +1,2 @@
 from server_config.settings import DEFAULT_TIMEOUT_SECONDS, BASE_DIR, DATABASES
+from server_config.database import Base, SQLALCHEMY_DATABASE_URL
---
+++ src/product_design_model/server_config/database.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+
+from server_config.settings import DATABASES
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{DATABASES['default']['HOST']}:{DATABASES['default']['PORT']}/{DATABASES['default']['NAME']}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=10, max_overflow=10)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        raise
+    finally:
+        session.close()
