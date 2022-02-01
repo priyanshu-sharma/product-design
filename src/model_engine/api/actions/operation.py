388c28da81e9a69e417f124c968771b5d5f37964
Priyanshu <priyanshu.sharma@grofers.com>
2022-02-01 13:33:33 +0530

    added try catch block for operations api

--- src/model_engine/api/actions/operation.py
+++ src/model_engine/api/actions/operation.py
@@ -23,14 +23,14 @@ router = APIRouter()
     """
 )
 async def operations(request: Request):
-    # try:
-    request_data = await request.json()
-    product_type = request_data.get("product_type", None)
-    schema = PRODUCT_TYPE_TO_SCHEMA_MAP[product_type]()
-    schema_data = schema.load(request_data)
-    print(schema.load(request_data))
-    result = {'schema_data': schema_data}
-    return {"status": "success", "result": result}
-    # except Exception as e:
-    #     logger.error("Error in Operations API: {}".format(e))
-    #     raise BadRequestException(e)
+    try:
+        request_data = await request.json()
+        product_type = request_data.get("product_type", None)
+        schema = PRODUCT_TYPE_TO_SCHEMA_MAP[product_type]()
+        schema_data = schema.load(request_data)
+        print(schema.load(request_data))
+        result = {'schema_data': schema_data}
+        return {"status": "success", "result": result}
+    except Exception as e:
+        logger.error("Error in Operations API: {}".format(e))
+        raise BadRequestException(e)
