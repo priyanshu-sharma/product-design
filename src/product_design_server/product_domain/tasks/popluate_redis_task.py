75ef1b1c97f7af1f80bf50b3472d2da98de49e4f
Priyanshu <priyanshu.sharma@grofers.com>
2022-03-02 13:20:03 +0530

    refactor get api

--- src/product_design_server/product_domain/tasks/popluate_redis_task.py
+++ src/product_design_server/product_domain/tasks/popluate_redis_task.py
@@ -6,8 +6,11 @@ from server_config import RedisClient
 @app.task(
     autoretry_for=(Exception,), retry_backoff=True, acks_late=True, queue="populate_redis_queue",
 )
-def populate_redis_task(handbag_detail_id):
+def populate_redis_task(handbag_details):
+    import pickle
     redis_client = RedisClient().get_instance()
-    handbag_detail = handbag_detail_api.get_handbag_details(handbag_detail_id)
-    redis_client.hmset(handbag_detail['name'], handbag_detail)
-    return {"status": "success", "id": handbag_detail_id}
+    for handbag_detail in handbag_details:
+        handbag_detail = handbag_detail_api.get_handbag_details(handbag_detail['id'])
+        pickled_handbag_detail = pickle.dumps(handbag_detail)
+        redis_client.set('ip_{}'.format(handbag_detail['name']), pickled_handbag_detail)
+    return {"status": "success", "message": "Handbag details populated successfully"}
