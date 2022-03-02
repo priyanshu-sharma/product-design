e79a528957bdccbd9dfd6578a38116a909db625d
Priyanshu <priyanshu.sharma@grofers.com>
2022-03-02 11:55:26 +0530

    added installation script

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+bash src/model_engine/entrypoint.sh
+bash src/product_design_server/entrypoint.sh
---
+++ src/model_engine/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pip uninstall --yes tensorflow, karpe
+pip install -r requirements.txt
+
+export CUDA_HOME=/usr/local/cuda
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64
+export PATH=$PATH:$CUDA_HOME/bin
+
+nvcc --version
