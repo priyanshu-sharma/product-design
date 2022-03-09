f5000b4f2844edbf39c5297949982e2cb7e715e4
Priyanshu <priyanshu.sharma@grofers.com>
2022-03-09 18:34:20 +0530

    hf css file import

--- src/product_design_ui/product_design_ui/src/pages/DashboardPage/DashboardPage.tsx
+++ src/product_design_ui/product_design_ui/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 import "./dashboard-page.css";
 import { Layout, Menu, Breadcrumb, PageHeader, Button } from "antd";
 import {
