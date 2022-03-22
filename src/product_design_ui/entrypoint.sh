c64f9778b90c4b21398233dc683ceb24277d509d
Priyanshu <priyanshu.sharma@grofers.com>
2022-03-22 12:01:00 +0530

    fix generated image path

--- src/product_design_ui/entrypoint.sh
+++ src/product_design_ui/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-cd product_design_ui/
+cd src/product_design_ui/product_design_ui/
+mkdir public/handbag
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.profile
+source ~/.bashrc
+sleep 4
+nvm --version
 nvm install 17.6.0
+sleep 4
 nvm run default --version
-node install
+node --version
+npm install
+npm install react-bootstrap bootstrap
+sleep 4
+npm start
